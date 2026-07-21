12be9a5cf799b2308ac3779c9e348100db4f3c02
tannud0987-stack <tannud0987@gmail.com>
2026-07-22 01:17:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for navigation
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
